9ffe489d5d25064840b56ff7334adab3fedbccf7
Nujsvart <kampfar@gmail.com>
2021-09-13 22:25:47 +0300

    Css Variables with JS

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll("input");
+
+const handleUpdate = e => {
+  const suffix = e.target.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${e.target.name}`,
+    `${e.target.value}${suffix}`
+  );
+};
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate));
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
