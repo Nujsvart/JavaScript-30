913b92fe85c466a1ec629b7eaf562c32fc4223ea
Nujsvart <kampfar@gmail.com>
2021-09-19 00:31:46 +0300

    String Times with Reduce

---
+++ 18 - Adding Up Times with Reduce/script.js
@@ -0,0 +1,20 @@
+const times = document.querySelectorAll("[data-time]");
+
+const seconds = [...times]
+  .map(time => time.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, cur) => total + cur, 0);
+console.log(seconds);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
