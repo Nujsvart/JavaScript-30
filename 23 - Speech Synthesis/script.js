a819b0b4299092df71b48c403e590ce8df16b795
Nujsvart <kampfar@gmail.com>
2021-09-20 00:50:07 +0300

    Speech Synthesis

---
+++ 23 - Speech Synthesis/script.js
@@ -0,0 +1,46 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+
+msg.text = document.querySelector('[name="text"]').value;
+
+// Handlers
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+
+    .map(
+      voice =>
+        `<option value="${voice.name}">${voice.lang} (${voice.lang})</option>`
+    )
+    .join("");
+}
+
+const setVoice = function () {
+  console.log(this.value);
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle();
+};
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) speechSynthesis.speak(msg);
+}
+
+function setOption(e) {
+  console.log(e.target.name, e.target.value);
+  msg[e.target.name] = e.target.value;
+  toggle();
+}
+
+// Event Listeners
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach(option => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
