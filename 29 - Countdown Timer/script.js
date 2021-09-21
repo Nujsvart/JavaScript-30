607e4b0ec1d83499f66d395e359ea00312415d45
Nujsvart <kampfar@gmail.com>
2021-09-21 15:44:12 +0300

    countdown timer

---
+++ 29 - Countdown Timer/script.js
@@ -0,0 +1,60 @@
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time");
+const alarm = document.querySelector("[data-ses]");
+
+let countdown;
+
+function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  displayTimeLeft(seconds);
+  displayEndTime(then);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      timerDisplay.textContent = "";
+      endTime.textContent = "Mola Bitti 😜";
+      alarm.play();
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = `${Math.floor(seconds / 60)}`;
+  const remainderSeconds = `${seconds % 60}`.padStart(2, "0");
+  const display = `${minutes}:${remainderSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = `${end.getHours()}`.padStart(2, "0");
+  const minutes = `${end.getMinutes()}`.padStart(2, "0");
+
+  endTime.textContent = `${hour}:${minutes}'de döneceğim.`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener("click", startTimer));
+
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
